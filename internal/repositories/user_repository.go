1afed49afac2f05f891eca758a7bde5ef9e73e53
Luan-Nguyen <143428158+luan-nguyen-huu@users.noreply.github.com>
2026-04-26 13:46:24 +0700

    Change NewUserRepository to return UserRepository pointer

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -11,7 +11,7 @@ type UserRepository struct {
 	db *gorm.DB
 }
 
-func NewUserRepository(db *gorm.DB) entities.UserRepositoryInterface {
+func NewUserRepository(db *gorm.DB) *UserRepository {
 	return &UserRepository{db: db}
 }
 
